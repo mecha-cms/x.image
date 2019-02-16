9d445f127cac0e8ca6649b999cb3bbf3327b54d2
Taufik Nurrohman <t.nurrohman77@gmail.com>
2019-02-16 20:49:42 +0700

    Update test-5.php

--- image/lot/asset/-test/test-5.php
+++ image/lot/asset/-test/test-5.php
@@ -2,5 +2,5 @@
 
 foreach ([10, 20, 30, 40, 50, 60, 70, 80, 90, 100] as $quality) {
     Image::open(__DIR__ . DS . '2.jpg')->resize(100)->saveTo($file = __DIR__ . DS . 'result' . DS . 'quality-test' . DS . $quality . '.jpg', $quality);
-    echo '<p>' . Asset::jpg($file) . '<br>' . (new File($file))->size() . '</p>';
+    echo '<p>' . Asset::jpg($file) . '<br>' . File::sizer(filesize($file)) . '</p>';
 }
