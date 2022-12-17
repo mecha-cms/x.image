e16b7efe72b9463188b7c9e8330d93e4eeba043a
Taufik Nurrohman <hi@taufik-nurrohman.com>
2022-12-17 20:50:47 +0700

    Update

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 if (!extension_loaded('gd')) {
-    abort('Missing %s extension.', ['PHP <a href="https://www.php.net/manual/en/book.image.php" rel="nofollow" target="_blank">gd</a>']);
+    abort(i('Missing %s extension.', ['PHP <a href="https://www.php.net/manual/en/book.image.php" rel="nofollow" target="_blank">gd</a>']));
 }
 
 require __DIR__ . D . 'engine' . D . 'f.php';
